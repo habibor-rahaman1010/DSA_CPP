22e261a174182497261bb8272cba66de2ec22735
habibor-rahaman1010 <habibor144369@gmail.com>
2022-12-23 18:49:58 +0600

    hello programmer

--- C++/program-3.cpp
+++ C++/program-3.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main() {
     cout << "Hello programmer! \n";
+    cout << "Hello c++ programmer. you know, so more power consume right now. \n";
 
-return 0;
+    return 0;
 }
