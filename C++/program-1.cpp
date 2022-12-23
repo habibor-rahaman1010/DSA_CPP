0fb2adc152f37e2414c2342fd09637a277140af3
habibor-rahaman1010 <habibor144369@gmail.com>
2022-12-23 18:29:06 +0600

    Start learn to C++

---
+++ C++/program-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int number[10] = {12, 34, 45, 56, 67, 78, 65, 66, 98, 33};
+
+    for(int i = 0; i < 10; i++){
+        int value = number[i];
+        cout << value << " ";
+    }
+
+return 0;
+}
---
+++ C++/program-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i, ara[10];
+
+    cout <<"Enter array element: ";
+    for(i = 0; i < 10; i++) {
+        cin >> ara[i];
+    }
+
+    for(i = 0; i < 10; i++){
+        cout << ara[i] << " ";
+    }
+
+return 0;
+}
