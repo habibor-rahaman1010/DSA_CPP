836513e0dcaeaa2646b94e2fb1bd3d00839652da
habibor-rahaman1010 <habibor144369@gmail.com>
2022-12-24 15:21:44 +0600

    hello programmer

---
+++ C++/program-4.cpp
@@ -0,0 +1,12 @@
+//new line in c++
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello programmer" << endl;
+    cout << "Programmer always want alone" << endl <<endl;
+    cout << "Everyone should learn program" <<endl;
+
+return 0;
+}
