21d5e565ebde7468b68b8202e33700218d0cb04e
habibor-rahaman1010 <habibor144369@gmail.com>
2022-12-24 15:39:16 +0600

    hello programmer

--- C++/program-5.cpp
+++ C++/program-5.cpp
@@ -1,1 +1,14 @@
+// variable in c++ program...
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 15;
+    string name = "Habibor Rahaman";
+
+    cout<< num <<"\n";
+    cout<< name <<"\n";
+
+return 0;
+}
