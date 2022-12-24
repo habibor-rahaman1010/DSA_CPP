9ddc889dbc91d6a7f567407fe6c92fe5d3918c94
habibor-rahaman1010 <habibor144369@gmail.com>
2022-12-24 16:06:30 +0600

    ok donr

--- C++/program-5.cpp
+++ C++/program-5.cpp
@@ -5,10 +5,16 @@ using namespace std;
 
 int main() {
     int num = 15;
+    char ch = 'H';
+    double x = 12.5;
     string name = "Habibor Rahaman";
+    bool terms = true;
 
     cout<< num <<"\n";
     cout<< name <<"\n";
+    cout<< ch <<"\n";
+    cout<< x <<"\n";
+    cout<< terms <<"\n";
 
 return 0;
 }
---
+++ C++/program-6.cpp
@@ -0,0 +1,14 @@
+// assign to multiple variable in a single line in c++ program...
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 25, y = 12, z = 32;
+    string fname = "Habibor", lname = "Rahaman";
+
+    cout<< x << " " << y << " " << z << endl;
+    cout<< fname << " " << lname << endl;
+
+return 0;
+}
