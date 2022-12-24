9e9f35245d9ddbb7d26cb96adca0127730164502
habibor-rahaman1010 <habibor144369@gmail.com>
2022-12-24 16:22:51 +0600

    ok done man

---
+++ C++/program-7.cpp
@@ -0,0 +1,18 @@
+// one value store in multiple variable in c program....
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y, z;
+    x = y = z = 20;
+    cout<< z << endl;
+
+    string name1, name2;
+    name1 = name2 = "Habibor Rahaman";
+
+    cout<< name1 << "\n";
+    cout<< name2 << "\n";
+
+return 0;
+}
---
+++ C++/program-8.cpp
@@ -0,0 +1,14 @@
+// string concatenation  in c++ program...
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string fname, lname;
+    fname = "Habibor";
+    lname = "Rahaman";
+
+    cout<< fname + " " + lname;
+
+return 0;
+}
