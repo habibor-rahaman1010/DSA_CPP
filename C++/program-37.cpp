e452b3a14c50f24f4654eac026dc2126f192e95d
habibor-rahaman1010 <habibor144369@gmail.com>
2022-12-26 14:57:46 +0600

    ok

--- C++/program-37.cpp
+++ C++/program-37.cpp
@@ -1,1 +1,23 @@
+// short hand if else statement in c++ program...
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    string ans = (n > 9) ? "True" : "False";
+    cout<< ans << endl;
+
+    bool cond = true;
+    string play = (cond) ? "I play football to you" : "I cant't football to you";
+    cout<< play << endl;
+
+    bool cond2 = true;
+    string play2 = (!cond2) ? "I play football to you" : "I cant't football to you";
+    cout<< play2 << endl;
+
+    int x = 5;
+    (x > 4) ? cout<< "True" << endl : cout<< "False" << endl;
+
+return 0;
+}
