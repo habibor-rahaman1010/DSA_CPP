32afd00ff16a6584e0a1c2269491ecdcb0431799
habibor-rahaman1010 <habibor144369@gmail.com>
2022-12-26 13:02:21 +0600

    ok

--- C++/program-28.cpp
+++ C++/program-28.cpp
@@ -1,1 +1,21 @@
+//max and min in c++ program...
 
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int maxValue = max(25, 32);
+    cout<< maxValue <<endl;
+
+    int maxx = max({25, 32, 56, 44, 31});
+    cout<< maxx <<endl;
+
+    int minn = min(23, 14);
+    cout<< minn <<endl;
+
+    int minNum = min({21, 14, 45, 22, 7, 8});
+    cout<< minNum <<endl;
+
+return 0;
+}
