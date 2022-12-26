195b82af2b646b21a43b99e42f11e375b67d4c4c
habibor-rahaman1010 <habibor144369@gmail.com>
2022-12-26 13:52:16 +0600

    ok

--- C++/program-33.cpp
+++ C++/program-33.cpp
@@ -1,1 +1,14 @@
+//boolean expression in c++ program...
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool isCoodingFun = true;
+    bool notFun = false;
+
+    cout<< isCoodingFun <<endl;
+    cout<< notFun <<endl;
+
+return 0;
+}
