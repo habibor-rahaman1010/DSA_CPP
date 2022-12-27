1f8a2db4043a184b51f56dbd25b1a4810f56c11c
habibor-rahaman1010 <habibor144369@gmail.com>
2022-12-27 20:07:40 +0600

    ok done

--- Basic_C++/program-76.cpp
+++ Basic_C++/program-76.cpp
@@ -1,1 +1,17 @@
+//reference in c++...
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    string food = "Chicken Fry";
+    string &meal = food;
+    cout<< food <<"\n";
+    cout<< meal << "\n";
+
+    //memory address to the variable...
+    cout<<&food <<"\n";
+    cout<<&meal <<"\n";
+
+return 0;
+}
