54bf10f98ef0616eeb317c35ca141a6ed91af333
habibor-rahaman1010 <habibor144369@gmail.com>
2022-12-29 20:52:06 +0600

    ok done

---
+++ array/program-4.cpp
@@ -0,0 +1,14 @@
+//string in c++ program...
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string text = "Hello habibor ";
+    string grit = "What's app man!";
+    string fullText = text + grit;
+
+    cout<<fullText <<"\n";
+
+return 0;
+}
