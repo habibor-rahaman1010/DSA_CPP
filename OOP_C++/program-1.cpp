6e5d939f7a7d925d43a15f8f063166ca056dba2a
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-01 20:11:28 +0600

    ok done

--- OOP_C++/program-1.cpp
+++ OOP_C++/program-1.cpp
@@ -4,7 +4,28 @@
 
 using namespace std;
 
+class Person {
+    public:
+        int id;
+        string name;
+        string address;
+        string email;
+};
+
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    Person obj;
+    obj.id = 14;
+    obj.name = "Habibor Rahaman";
+    obj.address = "Mohammodpur - Dhaka";
+    obj.email = "habibor.rahaman1010@gmail.com";
+
+    cout<<"id: " <<obj.id <<"\n";
+    cout<<"name: " <<obj.name <<"\n";
+    cout<<"address: " <<obj.address <<"\n";
+    cout<<"email: " <<obj.email <<"\n";
 
 return 0;
 }
