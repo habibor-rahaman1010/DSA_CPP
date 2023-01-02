ef1a65fc851b36b86bad04c9e1d43350a77b3f3c
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-02 23:49:13 +0600

    Learn OOP Right Now

--- OOP_C++/program-36.cpp
+++ OOP_C++/program-36.cpp
@@ -1,9 +1,9 @@
-
-// Recursive class in c++ OOP concept...and dynamic object creation and vector of class...
+// Recursive class in c++ OOP concept...and dynamic object creation and vector of class and sort array of class...
 
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Student {
@@ -60,6 +60,12 @@ class Student {
         }
 };
 
+//sort object...
+bool comp(Student a, Student b) {
+    //when a is less then b;
+    return a.id < b.id;
+}
+
 int main() {
 /*
     Student *s1 = new Student(14, "Habibor Rahaman", "01768280237", "hbaibor.rahaman1010@gmail.com", 23, "Morning", true);
@@ -73,10 +79,18 @@ int main() {
         s.push_back(Student(5 - i, "Habibor Rahaman", "01768280237", "hbaibor.rahaman1010@gmail.com", 23, "Morning", true));
     }
 
-    for(int i = 0; i < s.size(); i++){
+    for(int i = 0; i < 5; i++){
         s[i].displayInformation();
         cout<<"\n";
     }
+
+    cout<<"________________After sorting_____________ \n";
+    sort(s.begin(), s.end(), comp);
+    for(int i = 0; i < 5; i++){
+        s[i].displayInformation();
+        cout<<"\n";
+    }
+
 
 return 0;
 }
