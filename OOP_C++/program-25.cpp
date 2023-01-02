12ebc39961d04f69b6e952187f6f176e2cabecc7
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-02 20:24:18 +0600

    Learn OOP Right Now

--- OOP_C++/program-25.cpp
+++ OOP_C++/program-25.cpp
@@ -4,8 +4,37 @@
 #include <string>
 using namespace std;
 
-int main() {
+class Student {
+    private:
+        int id;
+        string name;
+        int age;
+        string email;
 
+    public:
+        void printInformation() {
+            cout<<"ID: " <<this->id <<"\n";
+            cout<<"Name: " <<this->name <<"\n";
+            cout<<"Age: " <<this->age <<"\n";
+            cout<<"Email: " <<this->email <<"\n";
+        }
+
+        void setInformation(int id, string name, int age, string email) {
+            this->id = id;
+            this->name = name;
+            this->age = age;
+            this->email = email;
+        }
+};
+
+int main() {
+    Student habib;
+    habib.setInformation(144369, "Habibor Rahaman", 23, "habibor.rahaman1010@gmail.com");
+    habib.printInformation();
+    cout<<"\n";
+    Student abdullah;
+    abdullah.setInformation(781378, "Abdullah Sheikh", 8, "abdullah.sheikh1@gmail.com");
+    abdullah.printInformation();
 
 return 0;
 }
