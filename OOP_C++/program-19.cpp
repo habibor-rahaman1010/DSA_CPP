f1d68315a8c707f3f009188ed27bb7987203f041
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-02 18:00:21 +0600

    ok done

--- OOP_C++/program-19.cpp
+++ OOP_C++/program-19.cpp
@@ -3,10 +3,49 @@
 #include <iostream>
 using namespace std;
 
-class
+class Animal {
+    public:
+        void animalSound(){
+            cout<<"The animal make a sound! \n";
+        }
+};
+
+class Dog : public Animal{
+    public:
+        void animalSound(){
+            cout<<"The dog doing sound ghew ghew! \n";
+        }
+};
+
+class Cat : public Animal{
+    public:
+        void animalSound(){
+            cout<<"The car doing sound Mew Mew! \n";
+        }
+};
+
+class Fox : public Animal{
+    public:
+        void animalSound(){
+            cout<<"The fox doing sound pakkawoa pakkawoa! \n";
+        }
+};
+
+
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
+    Animal myAnimal;
+    Dog myDog;
+    Cat myCat;
+    Fox myFox;
+
+    myAnimal.animalSound();
+    myDog.animalSound();
+    myCat.animalSound();
+    myFox.animalSound();
 
 return 0;
 }
