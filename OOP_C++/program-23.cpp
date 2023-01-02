36365de2b9f071ee29a7a338e7f46d229bc534bf
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-02 19:02:32 +0600

    I am learning OOP programming

--- OOP_C++/program-23.cpp
+++ OOP_C++/program-23.cpp
@@ -1,1 +1,36 @@
+// Recursive class in c++ OOP concept...
 
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Person {
+    public:
+        string name;
+        Person *father;
+        Person *mother;
+
+        //display information...
+        void displayInfo() {
+            cout<<"Name: " << this->name <<"\n";
+            cout<<"Father Name: " <<this->father->name <<"\n";
+            cout<<"Mother Name: " <<this->mother->name <<"\n";
+        }
+};
+
+int main() {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+
+        Person p;
+        p.father = new Person;
+        p.mother = new Person;
+
+        p.name = "Habibor Rahaman";
+        p.father->name = "Kiron Bepary";
+        p.mother->name = "Piyara Begum";
+
+        p.displayInfo();
+
+return 0;
+}
