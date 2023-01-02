ec54ac7ed7ee513ba58f1a0c130d91bdb8651419
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-02 10:55:23 +0600

    i'm learning oop

--- OOP_C++/program-8.cpp
+++ OOP_C++/program-8.cpp
@@ -27,10 +27,23 @@ class Computer {
             cout<<"Password: " <<password <<"\n";
         }
 
+        //setter private data...
+        void setPrivateData(string userName, string email, string password) {
+            this->userName = userName;
+            this->email = email;
+            this->password = password;
+        }
+
         //getter protected data...
         void getProtectedData() {
             cout<<"License Key: " <<licenseKey <<"\n";
             cout<<"Expire Date: " <<expireDate <<"\n";
+        }
+
+        //setter protected data...
+        void setProtectedData(string licenseKey, string expireDate) {
+            this->licenseKey = licenseKey;
+            this->expireDate = expireDate;
         }
 
         //print public data
@@ -68,10 +81,14 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    Computer myPc("Habibor Rahaman", "habibor.rahaman1010@gmail.com",
-                  "R374$Zz2%J!MWX@h", "Rn79S20&BM740DPXHT6%E@uwH",
+    Computer myPc("Tamim Ahmed", "tamimahmed123@gmail.com",
+                  "daioeiweh", "0hwe923er9d3e9d3w",
                   "30-11-2023", "Core i5", "16GB", "1TB", "Thermal tech"
                   );
+
+    //reset data private and protected...
+    myPc.setPrivateData("Habibor Rahaman", "habibor.rahaman1010@gmail.com", "R374$Zz2%J!MWX@h");
+    myPc.setProtectedData("Rn79S20&BM740DPXHT6%E@uwH", "31-12-2025");
 
     //print all attribute here print by custom methods...
     myPc.getPrivateData();
