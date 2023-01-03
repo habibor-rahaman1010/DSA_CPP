21d2cc66543a11ae118e661a495f3e8c55c0955f
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-03 21:29:29 +0600

    Learn Searching Algorithm

--- Searching_Algorithms/program-5.cpp
+++ Searching_Algorithms/program-5.cpp
@@ -10,16 +10,43 @@ int main() {
     cout<<"Enter the value of (n and q): ";
     cin>>n >>q;
 
+    //for array value....
+    cout<<"Enter array value: ";
     vector<int>ara(n);
     for(int i = 0; i < ara.size(); i++){
         cin>>ara[i];
     }
     sort(ara.begin(), ara.end());
-    for(int i = 0; i < ara.size(); i++){
-        cout<<ara[i] <<" ";
-    }
 
     //for query value....
+    cout<<"Enter query value: ";
+    vector<int>query(q);
+    for(int i = 0; i < query.size(); i++){
+        cin>>query[i];
+    }
+
+    int low = 0, heigh = n - 1;
+    bool flag = 0;
+    while(low <= heigh){
+        int mid = (low + heigh) / 2;
+        for(int i = 0; i < query.size(); i++){
+            if(ara[mid] == query[i]){
+                cout<<"Yes! \n";
+                flag = 1;
+                return 0;
+            }
+
+            if(ara[mid] > query[i]){
+                heigh = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+    }
+    if(!flag){
+        cout<<"No! \n";
+    }
 
 return 0;
 }
