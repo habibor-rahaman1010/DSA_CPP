787bedce5358b7818ede910b337be1eeec75be33
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-03 21:05:42 +0600

    Learn Searching Algorithm

--- Searching_Algorithms/program-5.cpp
+++ Searching_Algorithms/program-5.cpp
@@ -1,1 +1,25 @@
+//binary search in c++ program....
 
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, q;
+    cout<<"Enter the value of (n and q): ";
+    cin>>n >>q;
+
+    vector<int>ara(n);
+    for(int i = 0; i < ara.size(); i++){
+        cin>>ara[i];
+    }
+    sort(ara.begin(), ara.end());
+    for(int i = 0; i < ara.size(); i++){
+        cout<<ara[i] <<" ";
+    }
+
+    //for query value....
+
+return 0;
+}
