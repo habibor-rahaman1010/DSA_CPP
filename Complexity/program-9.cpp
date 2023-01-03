7064cfa9fffaba07d71db817ad0903a735998232
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-03 14:14:49 +0600

    ok

--- Complexity/program-9.cpp
+++ Complexity/program-9.cpp
@@ -1,11 +1,29 @@
 //Linear search in c++ program...
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
+    int n, k;
+    cout<<"Enter the value of (n and k): ";
+    cin>>n >>k;
 
+    vector<int>ara(n);
+
+    for(int i = 0; i < ara.size(); i++){
+        cin>>ara[i];
+    }
+
+    string flag = "No! \n";
+    for(int i = 0; i < ara.size(); i++){
+        int value = ara[i];
+        if(value == k){
+            flag = "Yes! \n";
+        }
+    }
+    cout<<flag;
 
 return 0;
 }
