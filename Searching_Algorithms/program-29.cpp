2bf1b13ee65d361aecc68f4d45eedcd5e46c26cc
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-05 00:06:14 +0600

    Learn Sort Algorithm

--- Searching_Algorithms/program-29.cpp
+++ Searching_Algorithms/program-29.cpp
@@ -1,3 +1,35 @@
 //prime number find out use sieve method and find complexity...
 
-#include <iosd>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+    Time complexity O(nlogn)
+    Space complexity O(n)
+*/
+
+int main() {
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+
+    vector<int>primes;
+    vector<bool>visited(n + 1);
+
+    for(int i = 2; i <= n; i++){
+        if(visited[i] == false){
+            primes.push_back(i);
+            for(int j = 2; i * j <= n; j++){
+                visited[i * j] = true;
+            }
+        }
+    }
+
+    for(int i = 0; i < primes.size(); i++){
+        cout<<primes[i] <<" ";
+    }
+    cout<<"\n";
+
+return 0;
+}
