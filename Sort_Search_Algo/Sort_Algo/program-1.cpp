f58f46dbce6d3cde1851326d0c80d8a0f5311465
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-05 20:47:41 +0600

    ok done

---
+++ Sort_Search_Algo/Sort_Algo/program-1.cpp
@@ -0,0 +1,43 @@
+//Selection sort algorithm applying in c++ program...
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void Selection_Srot(vector<int>ara) {
+    int temp;
+    for(int step = 0; step < ara.size() - 1; step++){
+        int min_index = step;
+        for(int j = step + 1; j < ara.size(); j++){
+            if(ara[j] < ara[min_index]){
+                min_index = j;
+            }
+        }
+        temp = ara[min_index];
+        ara[min_index] = ara[step];
+        ara[step] = temp;
+    }
+
+    for(int i = 0 ; i < ara.size(); i++){
+        cout<<ara[i] <<" ";
+    }
+}
+
+int main() {
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+
+    vector<int>ara(n);
+    for(int i = 0; i < ara.size(); i++){
+        cin>>ara[i];
+    }
+
+    Selection_Srot(ara);
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+return 0;
+}
