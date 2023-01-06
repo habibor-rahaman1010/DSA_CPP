f7fdad536c287dda025f8976dc01728673ee6f96
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-06 16:56:04 +0600

    Hello programmer do practice more

--- Sort_Search_Algo/Sort_Algo/program-16.cpp
+++ Sort_Search_Algo/Sort_Algo/program-16.cpp
@@ -1,1 +1,41 @@
+//bubble sort algorithm descending order in c++ program...
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void BubbleSort(vector<int>ara) {
+    for(int i = 0; i < ara.size(); i++){
+        int last = ara.size() - 1 - i;
+        for(int j = 0; j < last; j++){
+            if(ara[j] > ara[j + 1]){
+                swap(ara[j], ara[j + 1]);
+            }
+        }
+    }
+
+    for(int i = 0; i < ara.size(); i++){
+        cout<<ara[i] <<" ";
+    }
+
+}
+
+int main() {
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+
+    vector<int>ara(n);
+    for(int i = 0; i < ara.size(); i++){
+        cin>>ara[i];
+    }
+
+    BubbleSort(ara);
+
+
+return 0;
+}
+
+
 
