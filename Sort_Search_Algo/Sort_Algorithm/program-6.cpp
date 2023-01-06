2d3bbaaf66af691addca752611466bf715a60403
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-06 19:26:49 +0600

    ok done programmer

---
+++ Sort_Search_Algo/Sort_Algorithm/program-6.cpp
@@ -0,0 +1,45 @@
+//bubble sort ascending order implement in c++ program...trying optimize..
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> Bubble_Sort(vector<int>ara) {
+    int temp;
+    bool isSorted;
+    for(int i = 0; i < ara.size(); i++){
+        isSorted = true;
+        for(int j = 1; j < ara.size(); j++){
+            if(ara[j] < ara[j - 1]){
+                temp = ara[j];
+                ara[j] = ara[j - 1];
+                ara[j - 1] = temp;
+                isSorted = false;
+            }
+        }
+        if(isSorted){
+            return ara;
+        }
+    }
+    return ara;
+}
+
+int main() {
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+
+    vector<int>ara(n);
+    for(int i = 0; i < ara.size(); i++){
+        cin>>ara[i];
+    }
+
+    vector<int>sortArray = Bubble_Sort(ara);
+    for(int i = 0; i < sortArray.size(); i++){
+       cout<<sortArray[i] <<" ";
+    }
+
+return 0;
+}
+
---
+++ Sort_Search_Algo/Sort_Algorithm/program-8.cpp
@@ -0,0 +1,44 @@
+//bubble sort ascending order implement in c++ program...trying optimize..
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> Bubble_Sort(vector<int>ara) {
+    int temp;
+    bool isSorted;
+    for(int i = 0; i < ara.size(); i++){
+        isSorted = true;
+        for(int j = 1; j < ara.size(); j++){
+            if(ara[j] < ara[j - 1]){
+                swap(ara[j], ara[j - 1]);
+                isSorted = false;
+            }
+        }
+        if(isSorted){
+            return ara;
+        }
+    }
+    return ara;
+}
+
+int main() {
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+
+    vector<int>ara(n);
+    for(int i = 0; i < ara.size(); i++){
+        cin>>ara[i];
+    }
+
+    vector<int>sortArray = Bubble_Sort(ara);
+    for(int i = 0; i < sortArray.size(); i++){
+       cout<<sortArray[i] <<" ";
+    }
+
+return 0;
+}
+
+
