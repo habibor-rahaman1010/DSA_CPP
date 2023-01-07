321c51b09374ec60cfb084491659c4aae6a44d99
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-07 19:52:32 +0600

    ok

--- Sort_Search_Algo/Sort_Algorithm/program-18.cpp
+++ Sort_Search_Algo/Sort_Algorithm/program-18.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 vector<int> Insertion_Sort(vector<int>ara) {
-    //[8, 2, 4, 1, 3]
+    //[8, 2, 4, 1, 3] [2 , 8, 4, 1, 3]
     for(int i = 1; i < ara.size(); i++){
         int current = ara[i];
         int j = i - 1;
