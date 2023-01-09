385639287e3432b3b52f0b26188d86a715aeed2f
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-09 12:54:34 +0600

    ok done

--- Merg_Sort/algoritham_problem/problem-1.cpp
+++ Merg_Sort/algoritham_problem/problem-1.cpp
@@ -1,2 +1,31 @@
 
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void sort_array(vector<int>sorted1, vector<int>sorted2) {
+     vector<int>merged;
+
+     for(int i = 0; i < sorted1.size(); i++){
+        merged.push_back(sorted1[i]);
+     }
+
+    for(int i = ; i < sorted2.size(); i++){
+        merged.push_back(sorted2[i]);
+    }
+
+    for(int i = 0; i < merged.size(); i++){
+        cout<<merged[i] <<" ";
+    }
+}
+
+int main() {
+    vector<int>sorted1 = {1, 3, 4, 7, 8};
+    vector<int>sorted2 = {10, 11, 21, 22, 13};
+
+
+    sort_array(sorted1, sorted2);
+
+return 0;
+}
