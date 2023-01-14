54ae08353f55e8bc5fb65b24342b33953898fa8c
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-14 19:43:22 +0600

    Linked List Practice

--- Linked_List/program-1.cpp
+++ Linked_List/program-1.cpp
@@ -1,1 +1,43 @@
+//Linked List implement in c++ program...
 
+#include <iostream>
+
+class node {
+    public:
+        int data;
+        node* next;
+};
+
+class LinkedList {
+    node* head;
+
+    LinkedList(){
+        head = NULL;
+    }
+
+    //Insert new value at head...
+    void InsertAtHead(int value) {
+
+    };
+
+    //print the liked list...
+    void Traverse() {
+
+    }
+
+    //search for single value...
+    void SearchDistinckValue(int value) {
+
+    }
+
+    //search all possible occurrence...
+    void SearchAllValue(int value) {
+
+    }
+};
+
+int main() {
+
+
+return 0;
+}
