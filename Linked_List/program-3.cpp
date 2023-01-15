3d3bd0620403b7128375d531f8863f32429273c9
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-15 19:03:24 +0600

    Currently i learning Linked List

--- Linked_List/program-3.cpp
+++ Linked_List/program-3.cpp
@@ -24,14 +24,79 @@ class LinkedList {
             return newNode;
         }
 
+        //Insert data at head in linked list...
         void InsertAtHead(int value) {
+            Node* a = CreateNewNode(value);
+            if(head == NULL){
+                head = a;
+                return;
+            }
+            a->next = head;
+            head = a;
+        }
 
+        // Traverse liked list
+        void Traverse() {
+            Node* a = head;
+            while(a != NULL){
+                cout<<a->data <<" ";
+                a = a->next;
+            }
+            cout<<"\n";
+        }
+
+        //Search distinct value in linked list...
+        int SearchDistinctValue(int value) {
+            Node* a = head;
+            int index = 0;
+
+            while(a != NULL){
+                if(a->data == value){
+                    return index;
+                }
+                a = a->next;
+                index++;
+            }
+            return - 1;
+        }
+
+        //Search all possible occurrence in lined list...
+        void SearchAllValue(int value) {
+            Node* a = head;
+            int index = 0;
+            bool flag = false;
+            while(a != NULL){
+                if(a->data == value){
+                    cout<<value <<" found it index " <<index <<"\n";
+                    flag = true;
+                }
+                index++;
+                a = a->next;
+            }
+            if(!flag){
+                cout<<value <<" not found it index " <<-1 <<"\n";
+            }
         }
 
 };
 
 int main() {
+    LinkedList l;
+    l.InsertAtHead(10);
+    l.InsertAtHead(12);
+    l.InsertAtHead(15);
+    l.InsertAtHead(17);
+    l.InsertAtHead(10);
+    l.InsertAtHead(10);
+    l.InsertAtHead(15);
+    l.InsertAtHead(32);
 
+    l.Traverse();
+
+    cout<<"15 found it index " <<l.SearchDistinctValue(15) <<"\n \n";
+
+    l.SearchAllValue(100);
+    l.SearchAllValue(10);
 
 return 0;
 }
