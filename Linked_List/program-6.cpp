e3098e8bc5ca845160d2f1f233d60f42c8a949f6
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-17 16:33:34 +0600

    Hello programer

--- Linked_List/program-6.cpp
+++ Linked_List/program-6.cpp
@@ -15,35 +15,32 @@ class Node {
 };
 
 //insert at head in linked list...
-void InsertAtHead(Node* &head, int value) {
+Node* InsertAtHead(Node* head, int value) {
     Node* new_node = new Node(value);
     if(head == NULL){
         head = new_node;
-        return;
+        return head;
     }
     new_node->Next = head;
     head = new_node;
+    return new_node;
 }
 
-//print the linked list...
 void Print_linked_List(Node* head){
     Node* a = head;
-    while(a != NULL){
-        cout<<a->Data <<" ";
-        a = a->Next;
-    }
 }
 
 int main() {
     Node* head = NULL;
-    InsertAtHead(head, 10);
-    InsertAtHead(head, 20);
-    InsertAtHead(head, 30);
-    InsertAtHead(head, 40);
-    InsertAtHead(head, 50);
-    InsertAtHead(head, 60);
+    head = InsertAtHead(head, 10);
+    head = InsertAtHead(head, 20);
+    head = InsertAtHead(head, 30);
+    head = InsertAtHead(head, 40);
 
-    Print_linked_List(head);
-
+    while(head != NULL){
+       cout<<head->Data <<" ";
+       head = head->Next;
+    }
 return 0;
 }
+
---
+++ Linked_List/program-7.cpp
@@ -0,0 +1,23 @@
+//conceptual session linked list practice
+
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int item){
+        this->Data = item;
+        this->Next = NULL;
+    }
+};
+
+
+
+int main() {
+
+
+return 0;
+}
