6434af0f268ee3387949d8440fd96bc29131b4c0
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-27 07:07:14 +0600

    ok programmer

---
+++ problem_solve2/program-1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void reverse(vector<int>ara){
+    for(int i = ara.size() - 1; i >= 0; i-- ){
+        cout<<ara[i] <<" ";
+    }
+    cout<<"\n";
+}
+
+int main() {
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    vector<int>ara(n); 
+    for(int i = 0; i < n; i++){
+        cin>>ara[i];
+    }
+
+    reverse(ara);
+
+return 0;
+}
