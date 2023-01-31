9267007aa6248e48667960467a836fe35dbe70a1
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-31 19:06:19 +0600

    hello programer

--- stack/program-3.cpp
+++ stack/program-3.cpp
@@ -21,8 +21,8 @@ public:
     Node* Create_Node(int item) {
         Node* new_node = new Node;
         new_node->data = item;
-        new_node->nxt = NULL;
-        new_node->prv = NULL;
+        new_node->next = NULL;
+        new_node->privious = NULL;
         return new_node;
     }
 
@@ -34,19 +34,23 @@ public:
             return;
         }
         Node* a = head;
-        new_node->nxt = a;
-        a->prv = new_node;
+        new_node->next = a;
+        a->privious = new_node;
         head = new_node;
     }
 
-    //Traverse the doubly linked list...
-    void Traverse(){
-        Node* a = head;
-        while(a != NULL){
-            cout<<a->data <<" ";
-            a = a->nxt;
+    //delete at head...
+    void deleteAtHead(){
+        if(head == NULL){
+            return;
         }
-        cout<<"\n";
+        Node *a = head;
+        Node *b  = head->next;
+        delete a;
+        if(b != NULL){
+            b->privious = NULL;
+        }
+        head = b;
     }
 
     //This function return to the doubly linked list length...
@@ -54,7 +58,7 @@ public:
         Node* a = head;
         int countt = 0;
         while(a != NULL){
-            a = a->nxt;
+            a = a->next;
             countt++;
         }
         return countt;
@@ -62,12 +66,47 @@ public:
 };
 
 class Stack{
+public:
+    DoublyLinkedList dl;
 
+    Stack() {
+
+    }
+
+    void push(int item){
+        dl.InsertAtHead(item);
+    }
+
+    void pop(){
+        if(dl.getLength() == 0){
+            cout<<"Stack is empty! \n";
+            return;
+        }
+        dl.deleteAtHead();
+    }
+
+    int top(){
+        if(dl.getLength() == 0){
+            cout<<"Stack is empty! \n";
+            return -1;
+        }
+        return dl.head->data;
+    }
 };
 
 int main() {
+    Stack st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
 
+    cout<<st.top() <<"\n";
+    st.pop();
+    cout<<st.top() <<"\n";
+    st.pop();
+    cout<<st.top() <<"\n";
+    st.pop();
+    cout<<st.top() <<"\n";
 
 return 0;
 }
-
