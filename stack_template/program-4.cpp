6f7e1774c7c6204322cd8d4897c3120e1b6204a4
habibor-rahaman1010 <habibor144369@gmail.com>
2023-02-01 13:58:57 +0600

    learn data structure

---
+++ stack_template/program-4.cpp
@@ -0,0 +1,82 @@
+//stack implement in c++ program using c++ template
+
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+template<class T>
+class Stack{
+public:
+    T *ara;
+    int capacity;
+    int stack_size;
+
+    Stack(){
+        ara = new T[1];
+        capacity = 1;
+        stack_size = 0;
+    }
+
+    //stack capacity increase here...
+    void Increase(){
+        T *temp;
+        temp = new T[capacity * 2];
+        for(int i = 0; i < capacity; i++){
+            temp[i] = ara[i];
+        }
+        swap(ara, temp);
+        delete[] temp;
+        capacity = capacity * 2;
+    }
+
+    //Add element in the stack...
+    void push(T item){
+        if(stack_size + 1 > capacity){
+            Increase();
+        }
+        stack_size = stack_size + 1;
+        ara[stack_size - 1] = item;
+    }
+
+    //Pop element in the stack...
+    void pop(){
+        if(stack_size == 0){
+            cout<<"Stack is empty! \n";
+            return;
+        }
+        stack_size = stack_size - 1;
+    }
+
+    //Top return the stack top most item...
+    T top(){
+        if(stack_size == 0){
+           cout<<"Stack is empty! \n";
+           T a;
+           return a;
+        }
+        return ara[stack_size - 1];
+    }
+};
+
+int main() {
+    Stack<char>st;
+    st.push('h');
+    st.push('q');
+    st.push('n');
+    st.push('c');
+
+    cout<<st.top() <<"\n";
+    st.pop();
+    cout<<st.top() <<"\n";
+    st.pop();
+    cout<<st.top() <<"\n";
+    st.pop();
+    cout<<st.top() <<"\n";
+    st.pop();
+    cout<<st.top() <<"\n";
+
+return 0;
+}
+
+
+
