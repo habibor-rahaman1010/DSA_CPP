2b7a18c46c396c6b17ad310551afb2bc947a4413
habibor-rahaman1010 <habibor144369@gmail.com>
2023-02-02 15:45:17 +0600

    ok

--- more_stack/static_stack/program-1.cpp
+++ more_stack/static_stack/program-1.cpp
@@ -3,13 +3,68 @@
 #include <iostream>
 using namespace std;
 
+const int MAX_SIZE = 500;
+
 class Stack{
+public:
+    int ara[MAX_SIZE];
+    int stack_size;
+
+    Stack(){
+        stack_size = 0;
+    }
+
+    //push an element in the stack... O(1)
+    void push(int item){
+        if(stack_size + 1 > MAX_SIZE){
+            cout<<"Stack is full! \n";
+            return;
+        }
+        stack_size = stack_size + 1;
+        ara[stack_size - 1] = item;
+    }
+
+    //pop an element in the stack... O(1)
+    void pop(){
+        if(stack_size == 0){
+            cout<<"Stack is empty! \n";
+            return;
+        }
+        ara[stack_size - 1] = 0;
+        stack_size = stack_size - 1;
+    }
+
+    //return top element in the stack... O(1)
+    int top(){
+        if(stack_size == 0){
+            cout<<"Stack is empty! \n";
+            return -1;
+        }
+        return ara[stack_size - 1];
+    }
 
 };
 
 
 int main() {
+    Stack st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
 
+    cout<<st.top()<<"\n";
+    st.pop();
+    cout<<st.top()<<"\n";
+    st.pop();
+    cout<<st.top()<<"\n";
+    st.pop();
+    cout<<st.top()<<"\n";
+    st.pop();
+    cout<<st.top()<<"\n";
+    st.pop();
+    cout<<st.top()<<"\n";
 
-
+return 0;
 }
