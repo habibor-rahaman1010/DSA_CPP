405a6e1fd2ec8f6f1267b767b51d2cd08e78c605
habibor-rahaman1010 <habibor144369@gmail.com>
2023-02-15 15:33:31 +0600

    ok

--- Binary_Search_Tree/BST_Insertion/program-1.cpp
+++ Binary_Search_Tree/BST_Insertion/program-1.cpp
@@ -49,13 +49,53 @@ public:
                 r = a->right->data;
                 q.push(a->right);
             }
+
+            cout<< "Node value: " <<a->data <<" Left Childe: " <<l <<" Right Childe: " <<r <<"\n";
         }
     }
 
+    //insert data in binary search tree...
+    void Insert(int item) {
+        Node *newNode = CreateNewNode(item);
+
+        if(root == NULL){
+            root = newNode;
+            return;
+        }
+
+        Node *current = root;
+        Node *privious = NULL;
+
+        while(current != NULL){
+            if(newNode->data > current->data){
+                privious = current;
+                current = current->right;
+            }
+            else{
+                privious = current;
+                current = current->left;
+            }
+
+            if(newNode->data > privious->data){
+                privious->right = newNode;
+            }
+            else{
+                privious->left = newNode;
+            }
+        }
+    }
 };
 
 int main() {
+    Binary_Search_Tree bst;
 
+    bst.Insert(6);
+    bst.Insert(4);
+    bst.Insert(3);
+    bst.Insert(5);
+    bst.Insert(7);
+    bst.Insert(8);
 
+    bst.BFS();
 return 0;
 }
