825cd1e93286fea433756ca94dd64d617bb30fc8
habibor-rahaman1010 <habibor144369@gmail.com>
2023-02-15 09:45:23 +0600

    ok done

--- Binary_Search_Tree/BST_Insertion/program-1.cpp
+++ Binary_Search_Tree/BST_Insertion/program-1.cpp
@@ -20,14 +20,37 @@ public:
     }
 
     Node *CreateNewNode(int item){
-        Node newNode = new Node;
+        Node *newNode = new Node;
         newNode->data = item;
         newNode->left = NULL;
         newNode->right = NULL;
         return newNode;
     }
 
+    void BFS(){
+        if(root == NULL){
+            return;
+        }
+        queue<Node *>q;
+        q.push(root);
 
+        while(!q.empty()){
+            Node *a = q.front();
+            q.pop();
+
+            int l = -1, r = -1;
+
+            if(a->left != NULL){
+                l = a->left->data;
+                q.push(a->left);
+            }
+
+            if(a->right != NULL){
+                r = a->right->data;
+                q.push(a->right);
+            }
+        }
+    }
 
 };
 
