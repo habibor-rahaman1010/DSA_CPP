e3e2e730ed96dc1eba535f00226454815fe70b2e
habibor-rahaman1010 <habibor144369@gmail.com>
2023-02-20 15:45:01 +0600

    Hello Programmer

--- Heap/program-2.cpp
+++ Heap/program-2.cpp
@@ -32,9 +32,17 @@ public:
             int left = 2 * index + 1;
             int right = 2 * index + 2;
 
-            if(){
-
+            if(left < nodes.size() && nodes[largest] < nodes[left]){
+                largest = left;
             }
+            if(right < nodes.size() && nodes[largest] < nodes[right]){
+                largest = right;
+            }
+            if(largest == index){
+                break;
+            }
+            swap(nodes[index], nodes[largest]);
+            index = largest;
         }
     }
 
@@ -50,18 +58,46 @@ public:
         for(int i = 0; i < nodes.size(); i++){
             cout<<nodes[i] << " ";
         }
-        cout<<" ";
+        cout<<"\n";
+    }
+
+    //get max value..
+    int getMax(){
+        if(nodes.empty()){
+            cout<<"Heap is empty! \n";
+            return -1;
+        }
+        return nodes[0];
+    }
+
+    //get max value and delete max value...
+    int ExtractMax(){
+        if(nodes.empty()){
+            cout<<"Heap is empty! \n";
+            return -1;
+        }
+        int store = nodes[0];
+        Delete(0);
+        return store;
     }
 };
 
 int main(){
-    MaxHeap heap;
+   MaxHeap  heap;
     heap.Insert(4);
     heap.Insert(7);
     heap.Insert(9);
     heap.Insert(1);
     heap.Insert(10);
+    heap.Insert(20);
+    heap.Insert(30);
 
+    heap.PrintHeap();
+    cout<<heap.getMax() <<"\n";
+
+    heap.Delete(0);
+    heap.PrintHeap();
+    cout<<heap.ExtractMax()<<"\n";
     heap.PrintHeap();
 
 
