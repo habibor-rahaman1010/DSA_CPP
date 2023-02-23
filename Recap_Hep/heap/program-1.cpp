847c7fa8336426be6091a877d17f6cb69f13c548
habibor-rahaman1010 <habibor144369@gmail.com>
2023-02-23 12:16:03 +0600

    Hello programmer

--- Recap_Hep/heap/program-1.cpp
+++ Recap_Hep/heap/program-1.cpp
@@ -9,9 +9,10 @@ public:
     vector<int>nodes;
 
     Heap(){
-
+        
     }
 
+    //this is my up_hepify function...
     void up_heapify(int index){
         while(index > 0 && nodes[index] > nodes[(index - 1) / 2]){
             swap(nodes[index], nodes[(index - 1) / 2]);
@@ -19,15 +20,45 @@ public:
         }
     }
 
-    void Insert(int x){
+    void Insert(int x ){
         nodes.push_back(x);
         up_heapify(nodes.size() - 1);
+    }
+
+    //print the heap
+    void print_heap(){
+        for(int i = 0; i < nodes.size(); i++){
+            cout<<nodes[i] <<" ";
+        }
+        cout<<" ";
+    }
+
+    //this is my down_heapify funtion...
+    void down_heapify(int index){
+
+    }
+
+    //delete heap node...
+    void Delete(int index){
+        swap(nodes[index], nodes[nodes.size() - 1]);
+        down_heapify(index);
+        nodes.pop_back();
     }
 
 };
 
 int main() {
-    cout<<"Hello programmer";
+    Heap h;
+
+    h.Insert(4);
+    h.Insert(7);
+    h.Insert(9);
+    h.Insert(1);
+    h.Insert(10);
+    h.Insert(5);
+    h.Insert(8);
+
+    h.print_heap();
 
 return 0;
 }
